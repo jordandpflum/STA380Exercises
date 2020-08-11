d8427c2c2124218463f63eaa8c38fafa11705ec9
Kessiena Ofunrein <40614624+kessiena@users.noreply.github.com>
2020-08-11 14:23:01 -0500

    time series plot

--- exercise 2/time_series_Kess.R
+++ exercise 2/time_series_Kess.R
@@ -3,8 +3,7 @@ library("dplyr")
 library(ggplot2)
 abia <- read.csv('ABIA.csv')
 
-#line plot month vs count of delayed
-  #plot avg delay time per month per airline
+#MILITARY TIME TO REGULAR AM/PM TIME FORMAT
 military.expected <- abia$CRSArrTime
 abia$ExpArrTime <- format(strptime(substr(as.POSIXct(sprintf("%04.0f", military.expected), 
                                                      format="%H%M"), 12, 16), '%H:%M'), '%I:%M ')
@@ -15,10 +14,20 @@ abia$ActArrTime <- format(strptime(substr(as.POSIXct(sprintf("%04.0f", military.
 #length of delay
 abia$delaytime <- abia$ActualElapsedTime - abia$CRSElapsedTime
 
+
+#LINE PLOT MONTH VS AVERAGE ARRIVAL DELAY TIME BY AIRLINE -----------------------------DONE 
 AvgArrDelay <- abia %>%
   group_by(Month, UniqueCarrier) %>%
   summarize(avgArrDelay = mean(ArrDelay, na.rm = TRUE))
-ggplot(data=AvgArrDelay, aes(x=Month, y=avgArrDelay, group=UniqueCarrier)) +
-  geom_line()+
-  geom_point()
+#maybe subset for popular airlines
+ggplot(AvgArrDelay, aes(x=Month, y=avgArrDelay, group=UniqueCarrier)) +
+  geom_line(aes(color=UniqueCarrier))+
+  geom_point(aes(color=UniqueCarrier))
+#---------------------------------------------------------------------------------------END
+
+#most popular airlines
+count(abia, vars = UniqueCarrier)
+
+
+
 
