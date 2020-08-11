8a36b2afd2071b6e036797a65401f54af91e2904
Kessiena Ofunrein <40614624+kessiena@users.noreply.github.com>
2020-08-11 13:52:20 -0500

    making time series plot

--- exercise 2/time_series_Kess.R
+++ exercise 2/time_series_Kess.R
@@ -1,17 +1,24 @@
+library(magrittr)
+library("dplyr")  
+library(ggplot2)
 abia <- read.csv('ABIA.csv')
 
 #line plot month vs count of delayed
   #plot avg delay time per month per airline
 military.expected <- abia$CRSArrTime
 abia$ExpArrTime <- format(strptime(substr(as.POSIXct(sprintf("%04.0f", military.expected), 
-                                                     format="%H%M"), 12, 16), '%H:%M'), '%I:%M %p')
+                                                     format="%H%M"), 12, 16), '%H:%M'), '%I:%M ')
 military.actual <- abia$ArrTime
 abia$ActArrTime <- format(strptime(substr(as.POSIXct(sprintf("%04.0f", military.expected), 
-                                                     format="%H%M"), 12, 16), '%H:%M'), '%I:%M %p')
+                                                     format="%H%M"), 12, 16), '%H:%M'), '%I:%M ')
 #QUESTION - what time zone is this in 
 #length of delay
-time1<- abia$ExpArrTime
-time2<- abia$ActArrTime
-abia$arrival.delay <- difftime(time1, time2)
+abia$delaytime <- abia$ActualElapsedTime - abia$CRSElapsedTime
 
-abia$arrival.delay <- difftime(time2, time1, units="mins")
+AvgArrDelay <- abia %>%
+  group_by(Month, UniqueCarrier) %>%
+  summarize(avgArrDelay = mean(ArrDelay, na.rm = TRUE))
+ggplot(data=AvgArrDelay, aes(x=Month, y=avgArrDelay, group=UniqueCarrier)) +
+  geom_line()+
+  geom_point()
+
