6522989465f297e0ed96fddc84555041be354a62
Kessiena Ofunrein <40614624+kessiena@users.noreply.github.com>
2020-08-13 21:17:49 -0500

    update plots

--- exercise1/exercise_1_Kess_Plots.R
+++ exercise1/exercise_1_Kess_Plots.R
@@ -2,11 +2,7 @@ library(mosaic)
 library(dplyr)
 library(ggplot2)
 
-
-
 greenData <- read.csv('greenbuildings.csv')
-
-
 
 # Subset into just green buildings (Green Buildings: Rent)
 greenBuildings <- greenData %>% filter(green_rating == 1)
@@ -34,32 +30,32 @@ hist(greenBuildings$Rent, 25, xlab = 'Rent Per Square Foot', main = 'Histogram o
 
 
 
-#density plot of one df (parameter 1) with mean line -------------------------------FIX
+#density plot of one df (parameter 1) with mean line -------------------------------DONE
 #Parameter 1: dataframe to use Ex: green_only
 #Parameter 2: title of the grpah as a string Ex: "Title"
 #add mean line from entire dataset
-rent.denplot <- function(df,TITLE){
+rent.denplot <- function(df,TITLE,SUBTITLE){
   attach(df)
   mean.column = mean(df$Rent)
   mean.data = mean(greenData$Rent)
   graph = df %>%
       ggplot(aes(x=Rent))+
-        geom_density(fill="blue",alpha=0.5)+
+        geom_density(fill="darkgreen",alpha=0.5)+
         scale_x_log10()+
-        geom_vline(xintercept = mean.column,size=1,color="blue")+
+        geom_vline(xintercept = mean.column,size=1,color="darkgreen")+
         geom_vline(xintercept = mean.data,size=1,color="black")+
-        labs(y="Density", title = TITLE)+
-        geom_text(aes(x=mean.column-1, label="Subset", y=1.5), colour="blue", angle=90, text=element_text(size=12)) +
+        labs(y="Density", title = TITLE, subtitle = "Mean rent of the average building is 28.41", caption = SUBTITLE)+
+        geom_text(aes(x=mean.column-1, label="Subset", y=1.5), colour="darkgreen", angle=90, text=element_text(size=12)) +
         geom_text(aes(x=mean.data-1, label="All", y=1.5), colour="black", angle=90, text=element_text(size=12))
   return(graph) 
 }
-rent.denplot(greenBuildings_Ideal, "Density Plot of Ideal Green Building's Rent")
-rent.denplot(nonGreenBuildings_Ideal, "Density Plot of Ideal Non-Green Building's Rent")
+rent.denplot(greenBuildings_Ideal, "Density Plot of Ideal Green Building's Rent", "Mean of the avergae ideal green building is 30.40")
+rent.denplot(nonGreenBuildings_Ideal, "Density Plot of Ideal Non-Green Building's Rent", "Mean of the avergae ideal non-green building is 37.53")
 
 #density plot of green and non green on the same graph -------------------------------FIX
 #add mean line of rent from both groups of buildings
-plot(density(greenBuildings_Ideal$Rent), col = "darkgreen", xlab = "Rent Per Square Foot")
-lines(density(nonGreenBuildings_Ideal$Rent),col = "red",title="Non Green Buildings")
+plot(density(greenBuildings_Ideal$Rent), col = "darkgreen", xlab = "Rent Per Square Foot",main="Density of Rent Prices for Green and NonGreen Bldgs")
+lines(density(nonGreenBuildings_Ideal$Rent),col = "black")
 
 
 
