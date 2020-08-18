96907255fea8eb98ee77fd4794055b43edcf8fbd
jenny2298 <68300695+jenny2298@users.noreply.github.com>
2020-08-17 21:51:52 -0500

    Update Q2_Jenny.R

--- exercise 2/Q2_Jenny.R
+++ exercise 2/Q2_Jenny.R
@@ -25,7 +25,16 @@ qplot(d3$CRSDepTime,
       alpha=I(.5))
 
 
+# Analysis of delay time based on time
+##This shows that there are more long delays in the afternoon
 
+d4 = abia  %>% filter(DepDelay >=30) %>%
+  group_by(CRSDepTime) %>%
+  summarize(DepDelay)
 
+d4.glm <- glm(cbind(DepDelay, 875 - DepDelay) ~ CRSDepTime, 
+                    data=d4, family=binomial(logit))
+
+plot(DepDelay ~ CRSDepTime, data=d4, main='Depature Delay Length Based on Time of Day', xlab='Time of Day (hhmm0', ylab='Departure Delay Time')
 
 
